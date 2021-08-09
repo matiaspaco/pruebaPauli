dd0c31e6cd342c6be8c73bed048ea39a84a6cd18
Paulinafridman <fridman.pauliasofia@gmail.com>
2021-08-08 23:43:37 -0300

    faltan 20 min para terminar a clase8

--- promesas/main.js
+++ promesas/main.js
@@ -46,7 +46,7 @@ const prtTime = (mensaje,cl) => cl (mensaje,new Date().toLocaleDateString())
 
 /*-----------------------test de la promesa---------------------------*/
 
-prtTime('inicio delay',console.warn)
+/*prtTime('inicio delay',console.warn)
 retardo(2000, 'Retardo de 2 seg de prueba')
 .then(rta => {
     console.log(rta)
@@ -54,4 +54,71 @@ retardo(2000, 'Retardo de 2 seg de prueba')
 })
 
 .catch(error => console.error(error))
+
+/*----------------------- race ----------------------*/
+
+
+/*prtTime('Inicio delay', console.warn)
+Promise.race([
+    retardo(4000, 'retardo de 4 seg de prueba'),
+    retardo(3000, 'retardo de 3 seg de prueba'),
+    retardo(6000, 'retardo de 6 seg de prueba')
+
+
+])
+.then(rta=>{
+    console.log('Ganó', rta)
+    prtTime('Fin delay', console.log)
+})
+.catch(error => console.error('error en carrera', error))
+
+
+/*----------------Promese all-------------------------*/
+
+prtTime('Inicio delay', console.warn)
+Promise.all([
+    retardo(4000, 'retardo de 4 seg de prueba'),
+    retardo(3000, 'retardo de 3 seg de prueba'),
+    retardo(6000, 'retardo de 6 seg de prueba')
+
+
+])
+.then(rta=>{
+    console.log('Ganó', rta)
+    prtTime('Fin delay', console.log)
+})
+.catch(error => console.error('error en carrera', error))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
